4cdbbca1c4fc9e94a39fa7537e61d80136f9d337
Jeanne <jeannefernanda.m@gmail.com>
2023-11-26 16:01:01 -0400

    implementando funcao doarItem

--- projeto/Doador/Doador.js
+++ projeto/Doador/Doador.js
@@ -1,7 +1,26 @@
 const Pessoa = require('../Pessoa/Pessoa')
 
 class Doador extends Pessoa{
-    
+
+    listaDeItens = []
+
+    doarItem(item){
+        if(this.verificarSeItemJaEstaNaLista === true){
+            
+        } else{
+            this.listaDeItens.push({Item: item, Doador: super.getNome()});
+            return 'Item cadastrado para doação'
+        }
+    }
+
+    verificarSeItemJaEstaNaLista(item){
+        for(let i = 0; i < this.listaDeItens.length; i++){
+            if(item === this.listaDeItens[i]){
+                return true;
+            }
+        }
+    }
 }
 
 module.exports = Doador
+
