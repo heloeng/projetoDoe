2569e685fb77c0d3ad1741d2aba5c87cd4c932e2
Jeanne <jeannefernanda.m@gmail.com>
2023-11-26 16:06:48 -0400

    remoção de verificação desnecessária

--- projeto/Doador/Doador.js
+++ projeto/Doador/Doador.js
@@ -5,20 +5,8 @@ class Doador extends Pessoa{
     listaDeItens = []
 
     doarItem(item){
-        if(this.verificarSeItemJaEstaNaLista === true){
-            
-        } else{
-            this.listaDeItens.push({Item: item, Doador: super.getNome()});
-            return 'Item cadastrado para doação'
-        }
-    }
-
-    verificarSeItemJaEstaNaLista(item){
-        for(let i = 0; i < this.listaDeItens.length; i++){
-            if(item === this.listaDeItens[i]){
-                return true;
-            }
-        }
+        this.listaDeItens.push({Item: item, Doador: super.getNome()});
+        return 'Item cadastrado para doação'
     }
 }
 
