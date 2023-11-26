b3b9877f21561351858fb5a2aa0f3a35fe5d532a
Jeanne <jeannefernanda.m@gmail.com>
2023-11-26 15:18:26 -0400

    teste da classe Pessoa

--- projeto/Pessoa/Pessoa.test.js
+++ projeto/Pessoa/Pessoa.test.js
@@ -20,4 +20,17 @@ describe('Testar classe Pessoa', () =>{
         expect(pessoa1.cadastrarPessoa('Ana', 34534234567, 'Rua V, n° 23', '(11) 99324-0433')).toBe(`Cadastro de Ana realizado com sucesso.`)
     })    
 
+    test('Verificar se esta ocorrendo erro ao tentar cadastrar pessoa que já está cadastrada', () =>{
+        //setup
+        const pessoa1 = new Pessoa()
+
+        //ação
+        pessoa1.cadastrarPessoa('Ana', 34534234567, 'Rua V, n° 23', '(11) 99324-0433')
+        
+        //verificação
+        expect(() => pessoa1.cadastrarPessoa('Ana', 34534234567, 'Rua V, n° 23', '(11) 99324-0433').toThrow('Pessoa já cadastrada'))
+
+
+    })
+
 })
