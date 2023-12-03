1cc726cb661d4081dd28a2ea455df86893b093b1
Heloiza Mendes <helocursos@gmail.com>
2023-12-03 01:30:52 -0300

    Implementação da Classe ItemDoado

---
+++ projeto/Item/ItemDoado.js
@@ -0,0 +1,16 @@
+
+const Item = require('./Item.js')
+
+class ItemDoado extends Item {
+
+
+    constructor(nome) {
+        super(nome, 'ItemDoado');
+        Item.listaItens.push(this);
+    }
+
+
+}
+
+module.exports = ItemDoado;
+
