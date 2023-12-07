42865318227781c5f44932465efd56435935786b
Heloiza Mendes <helocursos@gmail.com>
2023-12-07 20:13:16 -0300

    remoção de comentários

--- projeto/Item/ItemSolicitado.js
+++ projeto/Item/ItemSolicitado.js
@@ -2,7 +2,6 @@ const Item = require("./Item");
 
 class ItemSolicitado extends Item {
   constructor(nome) {
-    //super da classe Item recebe nome e status
     super(nome, "Item Solicitado");
     Item.listaItens.push(this);
   }
